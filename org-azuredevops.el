eb4be5232f86ece2d429f471a869ee8769732baf
Chris Bilson <cbilson@microsoft.com>
2023-01-20 12:03:51 -0800

    test for another PR example
    ...but the real issue was that I was calling the wrong function in the template.

--- org-azuredevops.el
+++ org-azuredevops.el
@@ -26,6 +26,23 @@
   "The default repo to use for AzDevops repo related links."
   :group 'org-link :type 'string)
 
+;;;
+;;; Shared functions
+;;;
+(defun ado-export (type id link-fn description format)
+  "Export an AzDevops work item.
+Uses LINK-FN to get a hyperlink, for Work item ID, of type TYPE,
+with DESCRIPTION from an Org file for FORMAT."
+  (let* ((type (or type "Work Item"))
+         (description (or description (concat type " #" id)))
+         (href (funcall link-fn id)))
+    (pcase format
+      (`html (format "<a target=\"_blank\" href=\"%s\">%s</a>" href description))
+      (`latex (format "\\href{%s}{%s}" href description))
+      (`texinfo (format "@uref{%s,%s}" href description))
+      (`ascii (format "%s (%s)" description href))
+      (_ href))))
+
 ;; -----------------------------------------------------------------------------
 ;; Azure-DevOps repo (source code) links.
 ;; -----------------------------------------------------------------------------
@@ -125,67 +142,67 @@ which links to a file in repository <repo>."
         (host (or org-azuredevops-host)))
     (concat "https://" host "/" org "/_workitems/edit/" path)))
 
-(defun org-azdevops-workitem-command (path &optional org host)
+(defun ado-workitem-command (path &optional org host)
   "Open an AzDevops work-item link to the work item PATH in the browser."
   (browse-url (ado-workitem-url path org host)))
 
-(defun org-azdevops-workitem-export (id description format)
+(defun ado-workitem-export (id description format)
   "Export an AzDevops Work Item link ID with DESCRIPTION to FORMAT."
   (ado-export "Work Item" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "workitem" :follow #'org-azdevops-workitem-command :export #'org-azdevops-workitem-export)
+(org-link-set-parameters "workitem" :follow #'ado-workitem-command :export #'ado-workitem-export)
 
-(defun org-azdevops-epic-export (id description format)
+(defun ado-epic-export (id description format)
   "Export an AzDevops Epic link ID with DESCRIPTION to FORMAT."
   (ado-export "Epic" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "epic" :follow #'org-azdevops-workitem-command :export #'org-azdevops-epic-export)
+(org-link-set-parameters "epic" :follow #'ado-workitem-command :export #'ado-epic-export)
 
-(defun org-azdevops-feature-export (id description format)
+(defun ado-feature-export (id description format)
   "Export an AzDevops Feature link ID with DESCRIPTION to FORMAT."
   (ado-export "Feature" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "feature" :follow #'org-azdevops-workitem-command :export #'org-azdevops-feature-export)
+(org-link-set-parameters "feature" :follow #'ado-workitem-command :export #'ado-feature-export)
 
-(defun org-azdevops-pbi-export (id description format)
+(defun ado-pbi-export (id description format)
   "Export an AzDevops PBI link ID with DESCRIPTION to FORMAT."
   (ado-export "PBI" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "pbi" :follow #'org-azdevops-workitem-command :export #'org-azdevops-pbi-export)
+(org-link-set-parameters "pbi" :follow #'ado-workitem-command :export #'ado-pbi-export)
 
-(defun org-azdevops-task-export (id description format)
+(defun ado-task-export (id description format)
   "Export an AzDevops Task link ID with DESCRIPTION to FORMAT."
   (ado-export "Task" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "task" :follow #'org-azdevops-workitem-command :export #'org-azdevops-task-export)
+(org-link-set-parameters "task" :follow #'ado-workitem-command :export #'ado-task-export)
 
-(defun org-azdevops-bug-export (id description format)
+(defun ado-bug-export (id description format)
   "Export an AzDevops Bug link ID with DESCRIPTION to FORMAT."
   (ado-export "Bug" id 'ado-workitem-url description format))
 
-(org-link-set-parameters "bug" :follow #'org-azdevops-workitem-command :export #'org-azdevops-bug-export)
+(org-link-set-parameters "bug" :follow #'ado-workitem-command :export #'ado-bug-export)
 
 ;; -----------------------------------------------------------------------------
 ;; Artifact and other link types.
 ;; -----------------------------------------------------------------------------
 
 ;; Build links
-(defun org-azdevops-build-link-to-url (path)
+(defun ado-build-link-to-url (path)
   "Expand a build link PATH into a URL in AzDevops."
   (concat "https://" org-azuredevops-host "/" org-azuredevops-organization "/_build/results?buildId=" path))
 
-(defun org-azdevops-build-command (path)
+(defun ado-build-command (path)
   "Open an AzDevops build link to the work item PATH in the browser."
-  (browse-url (org-azdevops-build-link-to-url path)))
+  (browse-url (ado-build-link-to-url path)))
 
-(defun org-azdevops-build-export (id description format)
+(defun ado-build-export (id description format)
   "Export an AzDevops Build link ID with DESCRIPTION to FORMAT."
-  (ado-export "Build" id 'org-azdevops-build-link-to-url description format))
+  (ado-export "Build" id 'ado-build-link-to-url description format))
 
-(org-link-set-parameters "build" :follow #'org-azdevops-build-command :export #'org-azdevops-build-export)
+(org-link-set-parameters "build" :follow #'ado-build-command :export #'ado-build-export)
 
 ;; Pull Request links
-(defun org-azdevops-pr-link-to-url (path)
+(defun ado-pr-link-to-url (path)
   "Expand a Azure-Compute PR link PATH into a URL in AzDevops.
 Links can be in the form of `pr:<number>` which links to
 PR #<number> in the default repo, or `pr:<repo>/<number>`,
@@ -203,56 +220,43 @@ which links to PR #<number> in repository <repo>."
          (id (nth 1 components)))
     (concat "https://" org-azuredevops-host "/" org-azuredevops-organization "/_git/" repo "/pullrequest/" id)))
 
-(defun org-azdevops-pr-command (id)
+(defun ado-pr-command (id)
   "Open an AzDevops PR link to the PR ID in the browser."
-  (browse-url (org-azdevops-pr-link-to-url id)))
+  (browse-url (ado-pr-link-to-url id)))
 
-(defun org-azdevops-pr-export (id description format)
+(defun ado-pr-export (id description format)
   "Export an AzDevops PR link ID with DESCRIPTION to FORMAT."
-  (ado-export "Pull Request" id 'org-azdevops-pr-link-to-url description format))
+  (ado-export "Pull Request" id 'ado-pr-link-to-url description format))
 
-(org-link-set-parameters "pr" :follow #'org-azdevops-pr-command :export #'org-azdevops-pr-export)
-
-(defun org-azdevops-pullrequest-title-to-link (title)
-  "Given an AzureDevOps Pull Request TITLE (the 'copy' button to the right of the title on the
-pull request view), create a pr: link, preserving the title."
-  (if (string-match "^Pull Request \\([[:digit:]]+\\): \\(.*\\)$" title)
-      (concat "pr:" (match-string 1 title) ": " (match-string 2 title))
-    title))
+(org-link-set-parameters "pr" :follow #'ado-pr-command :export #'ado-pr-export)
 
 ;; Release links
-(defun org-azdevops-release-command (path)
+(defun ado-release-command (path)
   "Open an AzDevops release link to the work item PATH in the browser."
-  (browse-url (org-azdevops-release-link-to-url path)))
+  (browse-url (ado-release-link-to-url path)))
 
-(defun org-azdevops-release-link-to-url (path)
+(defun ado-release-link-to-url (path)
   "Expand a release link PATH into a URL in AzDevops."
   (concat "https://" org-azuredevops-host "/" org-azuredevops-organization
           "/_releaseProgress?_a=release-pipeline-progress&releaseId=" path))
 
-(defun org-azdevops-release-export (id description format)
+(defun ado-release-export (id description format)
   "Export an AzDevops Release link ID with DESCRIPTION to FORMAT."
-  (ado-export "Release" id 'org-azdevops-release-link-to-url description format))
+  (ado-export "Release" id 'ado-release-link-to-url description format))
 
-(org-link-set-parameters "release" :follow #'org-azdevops-release-command :export #'org-azdevops-release-export)
+(org-link-set-parameters "release" :follow #'ado-release-command :export #'ado-release-export)
 
-;; -----------------------------------------------------------------------------
-;; Public Functions
-;; -----------------------------------------------------------------------------
+;;; -----------------------------------------------------------------------------
+;;; Public Functions
+;;; -----------------------------------------------------------------------------
+(defun org-azuredevops-pullrequest-title-to-link (title)
+  "Given an AzureDevOps Pull Request TITLE (the 'copy' button to the right of the title on the
+pull request view), create a pr: link, preserving the title. Used in pull-request-capture-template.org"
+  (if (string-match "^Pull Request \\([[:digit:]]+\\): \\(.*\\)$" title)
+      (concat "pr:" (match-string 1 title) ": " (match-string 2 title))
+    title))
 
-(defun org-azuredevops-export (type id link-fn description format)
-  "Export an AzDevops work item.
-Uses LINK-FN to get a hyperlink, for Work item ID, of type TYPE,
-with DESCRIPTION from an Org file for FORMAT."
-  (let* ((type (or type "Work Item"))
-         (description (or description (concat type " #" id)))
-         (href (funcall link-fn id)))
-    (pcase format
-      (`html (format "<a target=\"_blank\" href=\"%s\">%s</a>" href description))
-      (`latex (format "\\href{%s}{%s}" href description))
-      (`texinfo (format "@uref{%s,%s}" href description))
-      (`ascii (format "%s (%s)" description href))
-      (_ href))))
+;;; -----------------------------------------------------------------------------
 
 (provide 'org-azuredevops)
 ;;; org-azuredevops.el ends here
--- tests/pr-link-tests.el
+++ tests/pr-link-tests.el
@@ -1,17 +1,20 @@
-;;; tests for Azure DevOps pr links  -*- lexical-binding: t -*-
+;;; tests for Azure DevOps pull request code links  -*- lexical-binding: t -*-
 (require 'buttercup)
 (require 'org-azuredevops)
 
 (describe
  "Converting Azure DevOps PR titles to links"
  (it "converts a good title to a link"
-     (expect (org-azdevops-pullrequest-title-to-link "Pull Request 123456789: Some really cool change")
+     (expect (org-azuredevops-pullrequest-title-to-link "Pull Request 123456789: Some really cool change")
              :to-equal
-             "pr:123456789: Some really cool change"))
+             "pr:123456789: Some really cool change")
+     (expect (org-azuredevops-pullrequest-title-to-link "Pull Request 7497240: Lifecycle - Assign resource to Decom owner when they go to PreparingDecom (could be after ReturnResource API call)")
+             :to-equal
+             "pr:7497240: Lifecycle - Assign resource to Decom owner when they go to PreparingDecom (could be after ReturnResource API call)"))
  (it "leaves bad titles as is"
-     (expect (org-azdevops-pullrequest-title-to-link "Blah 123456789: Some really cool change")
+     (expect (org-azuredevops-pullrequest-title-to-link "Blah 123456789: Some really cool change")
              :to-equal "Blah 123456789: Some really cool change")
-     (expect (org-azdevops-pullrequest-title-to-link "Pull Request: Some really cool change")
+     (expect (org-azuredevops-pullrequest-title-to-link "Pull Request: Some really cool change")
              :to-equal "Pull Request: Some really cool change")))
 
 ;; Local Variables:
